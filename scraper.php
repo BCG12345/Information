5059658fa8cf6e4846626230e7375961b208c94c
BCG12345 <30796091+BCG12345@users.noreply.github.com>
2017-08-07 14:46:06 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,7 +1,6 @@
 <?
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
-
 // require 'scraperwiki.php';
 // require 'scraperwiki/simple_html_dom.php';
 //
@@ -18,10 +17,46 @@
 //
 // // An arbitrary query against the database
 // scraperwiki::select("* from data where 'name'='peter'")
-
 // You don't have to do things with the ScraperWiki library.
 // You can use whatever libraries you want: https://morph.io/documentation/php
 // All that matters is that your final data is written to an SQLite database
 // called "data.sqlite" in the current working directory which has at least a table
-// called "data".
+// called "data".3
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+$Alpha	=	array('AB');
+$url = 'http://islamabadexcise.gov.pk/VEH_REG/VEH_QUERY.asp?X=';
+for ($outterloop = 0; $outterloop < sizeof($Alpha); $outterloop++) 
+{
+for ($innerloop = 100; $innerloop <103; $innerloop++) 
+{
+$NewLink	=	$url . $Alpha[$outterloop] . '&Y=' . $innerloop;
+$html 		= file_get_html($NewLink);
+foreach($html->find("/html/body/div/table/tbody/tr[1]/td/form/table/tbody/tr[7]/td/table/tbody") as $element)
+{
+	if($element)
+	{
+		echo $reg_no 		= $element->find("tr/td[2]/font" ,1)->plaintext;
+		echo $reg_date 		= $element->find("tr/td[2]/font" ,2)->plaintext;
+		echo $maker 		= $element->find("tr/td[2]/font" ,4)->plaintext;
+		echo $model 		= $element->find("tr/td[2]/font" ,6)->plaintext;
+		echo $chassis_no	= $element->find("tr/td[2]/font" ,8)->plaintext;
+		echo $engine_no 	= $element->find("tr/td[2]/font" ,10)->plaintext;
+		echo $owner 		= $element->find("tr/td[2]/font" ,12)->plaintext;
+		echo $sw 			= $element->find("tr/td[2]/font" ,14)->plaintext;
+		echo $type 			= $element->find("tr/td[2]/font" ,16)->plaintext; 
+		echo $NewLink;
+		if($reg_no != null)
+		{
+				scraperwiki::save_sqlite(array('name'), array('name' => $reg_no , 'regdate' => $reg_date, 'maker' => $maker, 'model' => $model, 'chas' => $chassis_no, 'engine' => $engine_no, 'owner' => $owner, 'sw' => $sw, 'type' => $type, 'link' => $NewLink));
+		}
+	}
+	
+	
+}
+}
+
+}
+	
+
 ?>
