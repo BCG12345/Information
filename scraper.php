3a7d00e7ee5b67c09f0ad2773b6e4b0f5aa63c22
BCG12345 <30796091+BCG12345@users.noreply.github.com>
2017-08-07 14:52:51 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -48,7 +48,7 @@ foreach($html->find("/html/body/div/table/tbody/tr[1]/td/form/table/tbody/tr[7]/
 		echo $NewLink;
 		if($reg_no != null)
 		{
-				scraperwiki::save_sqlite(array('name'), array('name' => '$reg_no' , 'regdate' => '$reg_date', 'maker' => '$maker', 'model' => '$model', 'chas' => '$chassis_no', 'engine' => '$engine_no', 'owner' => '$owner', 'sw' => '$sw', 'type' => '$type', 'link' => '$NewLink'));
+				scraperwiki::save_sqlite(array('name'), array(name => '$reg_no' , regdate => '$reg_date', maker => '$maker', model => '$model', chas => '$chassis_no', engine => '$engine_no', owner => '$owner', sw => '$sw', type => '$type', link => '$NewLink'));
 		}
 	}
 	
